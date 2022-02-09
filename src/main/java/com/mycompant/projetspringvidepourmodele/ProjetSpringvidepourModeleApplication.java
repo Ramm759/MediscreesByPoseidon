7c4c6c1a99966322adcb8d232abd9f051c114fe9
Ramm759 <fredgsn59@gmail.com>
2022-02-09 16:17:04 +0100

    Rebase du projet aec le modele projet vide

---
+++ src/main/java/com/mycompant/projetspringvidepourmodele/ProjetSpringvidepourModeleApplication.java
@@ -0,0 +1,13 @@
+package com.mycompant.projetspringvidepourmodele;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetSpringvidepourModeleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetSpringvidepourModeleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mycompant/projetspringvidepourmodele/ProjetSpringvidepourModeleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompant.projetspringvidepourmodele;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSpringvidepourModeleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
